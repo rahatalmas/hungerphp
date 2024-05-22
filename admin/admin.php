dd482bf94d035752e6c7c4d69fd1c3286dfa6557
Akatsuki008 <pretty15-14395@diu.edu.bd>
2024-05-22 11:41:22 +0600

    newnew

---
+++ admin/admin.php
@@ -0,0 +1,43 @@
+<?php
+    include '../models/database.php';
+    include '../models/foodModel.php';
+    $jsonData = get_foods();
+    $foods = json_decode($jsonData);
+    //$featuredfoods = array_slice($foods,0,4);
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../cssfiles/banner.css">
+    <link rel="stylesheet" href="../cssfiles/itemcard.css">
+    <link rel="stylesheet" href="../cssfiles/apppromote.css">
+    <link rel="stylesheet" href="../cssfiles/headerpart.css">
+
+</head>
+<body>
+     <?php
+     include '../views/header.php';
+     ?>
+     <div id="food-section-items" class="section-items">
+        <?php
+          foreach ($foods as $food) {
+              echo  
+              "<div class=\"food-card\">
+              <img class=\"food-card-img\" style=\"width: 100%;\" src=\"$food->food_picture\"/>
+              <div class=\"food-card-description\">
+                  <h3>$food->food_name</h3>
+                  <p>$food->food_description</p>
+                  <h4>$food->hotel_name</h4>
+                  <h4 class=\"price-tag\">Price: $food->food_price Tk</h4>
+                  <button class=\"food-card-add-btn\">Add To Card</button>
+              </div>
+              </div>
+              ";
+          }
+        ?>
+   </div>
+</body>
+</html>
